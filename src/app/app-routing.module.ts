6cddf94ea157f1df1b8adac97d52d27d945f959f
Laura Almirón <lauraandreaalmiron12@gmail.com>
2025-03-09 00:45:48 -0300

    add modules and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SubscriptionOptionsComponent } from './subscription/subscription-options/subscription-options.component';
+import { UserDetailsComponent } from './user/user-details/user-details.component';
+import { PaymentDetailsComponent } from './payment/payment-details/payment-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SubscriptionOptionsComponent },
+  { path: 'user', component: UserDetailsComponent },
+  { path: 'payment', component: PaymentDetailsComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SubscriptionModule } from './subscription/subscription.module';
+import { UserModule } from './user/user.module';
+import { PaymentModule } from './payment/payment.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SubscriptionModule,
+    UserModule,
+    PaymentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
