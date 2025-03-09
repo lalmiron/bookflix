a988d8d00bd2d08a271ec1fce14eed44a5ecb0a7
Laura Almirón <lauraandreaalmiron12@gmail.com>
2025-03-09 00:42:36 -0300

    component subscription-options

---
+++ src/app/subscription/subscription-options/subscription-options.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription-options',
+  templateUrl: './subscription-options.component.html',
+  styleUrls: ['./subscription-options.component.css']
+})
+export class SubscriptionOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/subscription/subscription.module.ts
+++ src/app/subscription/subscription.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SubscriptionRoutingModule } from './subscription-routing.module';
+import { SubscriptionOptionsComponent } from './subscription-options/subscription-options.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SubscriptionOptionsComponent
+  ],
   imports: [
     CommonModule,
     SubscriptionRoutingModule
