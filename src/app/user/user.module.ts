b3b00d3a0534013c384f1e7ac1881353f60816f9
Laura Almirón <lauraandreaalmiron12@gmail.com>
2025-03-09 00:43:15 -0300

    component user-details

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserDetailsComponent
+  ],
   imports: [
     CommonModule,
     UserRoutingModule
