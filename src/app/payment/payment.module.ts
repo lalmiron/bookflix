05065155b0181891cba916cda11b802202a3bb6e
Laura Almirón <lauraandreaalmiron12@gmail.com>
2025-03-09 00:43:55 -0300

    component payment-details

--- src/app/payment/payment.module.ts
+++ src/app/payment/payment.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PaymentRoutingModule } from './payment-routing.module';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PaymentDetailsComponent
+  ],
   imports: [
     CommonModule,
     PaymentRoutingModule
